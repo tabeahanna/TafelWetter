18160706ed1eeff2d91a0ca12bf2d3e8854ed16d
tabeahanna <99529141+tabeahanna@users.noreply.github.com>
2022-03-09 17:23:59 +0100

    finish style

--- src/script.js
+++ src/script.js
@@ -144,7 +144,7 @@ function displayForecast(response) {
             )}</div>
             <img src="http://openweathermap.org/img/wn/${
               forecastDay.weather[0].icon
-            }@2x.png" alt="" width="36"  >
+            }@2x.png" alt="" width="80"  >
          <div class="weather-forecast-temperatures">
            <span class="weather-forecast-temperature-max">${Math.round(
              forecastDay.temp.max
